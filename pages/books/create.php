e6d5840c7e7ef136f62efa6d208439dd8a0ea38e
andreimdrs <andreimdrs19@gmail.com>
2023-06-30 10:17:37 -0300

    commitado

--- pages/books/create.php
+++ pages/books/create.php
@@ -1,5 +1,14 @@
 <?php
 
+ if (! hasUser()) {
+    header('Location: /');
+ }
+
+ $method = $_SERVER['REQUEST_METHOD'];
+ if ($method == 'GET') {
+    include __DIR__ . '/../../pages/books/create.php';
+ }
+
 ?>
 
 <!DOCTYPE html>
@@ -11,13 +20,7 @@
     <title>Criar Livro</title>
 </head>
 <body>
-    <h1>Criar Livro</h1>
-    <form action="">
-        <label for="title">Title</label>
-        <input type="text" name="title" placeholder="Livro">
-        <label for="user">User</label>
-        <input type="text" name="user" placeholder="Usuário">
-        <button type="submit">Enviar</button>
-    </form>
+    
 </body>
 </html>
+
